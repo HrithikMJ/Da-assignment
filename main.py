b491f5cb5031e5b489925271a8494a355a8f6a3f
Hrithik MJ <hrithikmj003@gmail.com>
2022-05-10 21:01:04 +0530

    update

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import math
-n=24
+n=15
+print("H0: u is from the large population")
+print("H1: u is not from the large population\n\n")
 df1=pd.read_csv("Kaggle_Upload.csv")
 m1=df1["Total Confirmed cases "].mean()
 m2=df1["Death"].mean()
@@ -11,5 +13,10 @@ se2=sd2/math.sqrt(n)
 ss1=se1**2
 ss2=se2**2
 sed = math.sqrt(ss1*ss2)
-t_stat = (m1 - m2) / sed
-print(t_stat)
+t = (m1 - m2) / sed
+print("Calculated t value =",t)
+
+if t < 1.96 and t > -1.96:
+        print("\nretain H0, this sample is from the large population")
+else:
+    print("\nreject H0,this sample is not from the large population")
